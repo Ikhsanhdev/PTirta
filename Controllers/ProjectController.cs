3644a6d6b9a13f3aaaf96808291c296409868e02
naffonly <nafjie02@gmail.com>
2025-02-04 15:28:16 +0700

    add project

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Higertech.Models;
+
+namespace Higertech.Controllers;
+
+public class ProjectController : Controller
+{
+    private readonly ILogger<ProjectController> _logger;
+
+    public ProjectController(ILogger<ProjectController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Detail()
+    {
+       return View();
+    }
+    
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
